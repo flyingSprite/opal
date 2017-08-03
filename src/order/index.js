74e41d6d44d0ba7da9a9ab6bdd50ee8ea1e92e4a
Quesle <eneblue@126.com>
2017-08-03 16:02:35 +0800

    [Improve] change spec files name and remove index.js file

--- src/order/index.js
+++
@@ -1,8 +0,0 @@
-export GenerateFolder from './order_1_GenerateFolderByTimestamp';
-export LoginSystem from './order_2_LoginSystemByHttps';
-
-
-// export default {
-//   GenerateFolder,
-//   LoginSystem
-// };
