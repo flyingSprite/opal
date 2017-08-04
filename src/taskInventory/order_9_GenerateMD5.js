aa2054aa101590236a5a3f59c42ea7869ea96df4
Quesle <eneblue@126.com>
2017-08-04 10:38:05 +0800

    [Task Inventory] Order 9: Generate MD5 for text

---
+++ src/taskInventory/order_9_GenerateMD5.js
@@ -0,0 +1,14 @@
+import crypto from 'crypto';
+
+/**
+ * Order 9: Generate MD5 for text
+ * Using utf-8 encode.
+ */
+class GenerateMD5 {
+
+  static md5(text) {
+    return crypto.createHash('md5').update(text).digest('hex');
+  }
+}
+
+export default GenerateMD5;
---
+++ test/order/order_9_GenerateMD5.spec.js
@@ -0,0 +1,6 @@
+
+import GenerateMD5 from  '../../src/taskInventory/order_9_GenerateMD5';
+
+const md5text = GenerateMD5.md5('123456');
+
+console.log(md5text); // e10adc3949ba59abbe56e057f20f883e
